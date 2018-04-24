b0bfa43a38011b03b45dbc4e00129c6e016d8c6b
David Morales <git@damorales.com>
2018-04-23 20:55:24 -0500

    Create and start express API server as proxy

--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,9 @@
-console.log(' ==== Running server ==== ');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
